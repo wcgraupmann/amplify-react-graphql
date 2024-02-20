3a3eaf99f2356ee8df0b909f1e0f3044e0af4b1d
wcgraupmann <141872356+wcgraupmann@users.noreply.github.com>
2024-02-20 10:21:42 -0600

    add GraphQl and Database

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqla83caa6e": {
       "AppClientID": "string",
